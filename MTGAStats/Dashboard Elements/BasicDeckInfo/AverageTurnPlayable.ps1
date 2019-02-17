836abaa4b0251f94d4837994e918b99545b49eed
MKletz <michael.kletz.27@gmail.com>
2019-02-17 01:32:23 -0600

    Updated sample simulation with game class

--- MTGAStats/Dashboard Elements/BasicDeckInfo/AverageTurnPlayable.ps1
+++ MTGAStats/Dashboard Elements/BasicDeckInfo/AverageTurnPlayable.ps1
@@ -2,81 +2,47 @@ using module MTGAStats
 param
 (
     [Deck]$Deck,
-    [String]$CardName,
-    [Boolean]$CardInOpeningHand = $true,
-    [int]$StartingHandSize = 7,
-    [Boolean]$OnPlay = $true
+    [String]$CardName
 )
 
-[int]$TurnCount = 1
-[System.Collections.ArrayList]$Library = $Deck.MainDeck
-[System.Collections.ArrayList]$Hand = @()
-[System.Collections.ArrayList]$Lands = @()
+$Game = [Game]::New($Deck)
+
+$Game.tutor($CardName)
+[Card]$CardToCast = $Game.hand[0]
+$Game.DrawOpeningHand()
 
 [Boolean]$CanPlay = $false
-[Boolean]$CardDrawn = $false
-[Card]$CardToCast = $Deck.MainDeck | Where-Object -Property "Name" -EQ -Value $CardName | Select-Object -First 1
-
-If($CardInOpeningHand)
-{
-    $StartingHandSize--
-    for ($i = 0; $i -lt ($Library.Count -1); $i++) {
-        If($Hand.Count -eq 0 -and $Library[$i].name -eq $CardName)
-        {
-            $Hand += $Library[$i]
-            $Library.RemoveAt($i)
-        }
-    }
-}
-1..$StartingHandSize | ForEach-Object -Process {
-    [int]$DeckIndex = Get-Random -Minimum 0 -Maximum ($Library.Count -1)
-    $Hand += $Library[$DeckIndex]
-    $Library.RemoveAt($DeckIndex)
-}
 
 While($CanPlay -eq $false)
 {
-    [Boolean]$PlayedLand = $false
+    $Game.StartTurn()
 
-    If( !($OnPlay -and $TurnCount -eq 1) )
+    If ( $Land = $Game.hand | Where-Object -Property type_line -Like -Value "*Land*" | Get-Random )
     {
-        $Index = Get-Random -Minimum 0 -Maximum ($Library.Count -1)
-        $Hand += $Library[$Index]
-        $Library.RemoveAt($Index)
-    }
-
-    for ($i = 0; $i -lt $Hand.Count; $i++)
-    {
-        If(!$PlayedLand -and $Hand[$i].type_line -like "*Land*" )
-        {
-            $Lands += $Hand[$i]
-            $Hand.RemoveAt($i)
-            $PlayedLand = $true
-
-        }
+        $Game.PlayCard($Land)
     }
 
     [String[]]$ManaProduction = @()
-    $Lands | ForEach-Object -Process {
-        $ManaProduction += $_.GetManaProduction()
+    [int]$ManaProducers = 0
+    $Game.BattleField | ForEach-Object -Process {
+        If($Mana = $_.GetManaProduction())
+        {
+            $ManaProduction += $Mana
+            $ManaProducers++
+        }
     }
 
     [System.Collections.ArrayList]$MissingColors = @()
     $MissingColors += Compare-Object -ReferenceObject $ManaProduction -DifferenceObject $CardToCast.GetManaColorRequirements() | Where-Object -Property "SideIndicator" -EQ "=>"
 
-    $CardDrawn = ($Hand).Name -contains $CardToCast.name
-
-    If( ($Lands.Count -ge $CardToCast.cmc) -and ($MissingColors.count -eq 0) -and $CardDrawn )
+    If( ($ManaProducers -ge $CardToCast.cmc) -and ($MissingColors.count -eq 0))
     {
         $CanPlay = $true
     }
     else
     {
-        $TurnCount++
+        $Game.EndTurn()
     }  
 }
 
-[PSCustomObject]@{
-    Turn = $TurnCount
-    LandsInPlay = $Lands.Count
-}
+Return ,$Game
